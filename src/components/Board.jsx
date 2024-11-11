9834a574da30caa45ed3fb898af5884b9e36e0b6
Daniel Gómez <danigb2904@gmail.com>
2024-11-11 13:31:23 +0100

    Lógica para actualizar el tablero añadido a board.js

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,37 +1,10 @@
-import { useState } from "react"
-import confetti from "canvas-confetti"
 import { Square } from "./Square"
 import { WinnerModal } from "./WinnerModal"
 import { TURNS } from "../constants"
-import { checkWinnerFrom, checkEndGame } from "../logic/board"
 import { useGameState } from "../logic/board"
 
-export function Board() {
-    
-    const {board, setBoard, turn, setTurns, winner, setWinner, resetGame } = useGameState();
-
-    const updateBoard = (index) => {
-        //Si en la posición del tablero hay algo que no sea nulo o 
-        //cuando se termina la partida 
-        //cierra la ejecución.
-
-        if (board[index] || winner) return
-
-        const newBoard = [...board]
-        newBoard[index] = turn
-        setBoard(newBoard)
-
-        const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-        setTurns(newTurn);
-
-        const newWinner = checkWinnerFrom(newBoard)
-        if (newWinner) {
-            confetti()
-            setWinner(newWinner);
-        } else if (checkEndGame(newBoard)) {
-            setWinner(false)
-        }
-    }
+export function Board() { 
+    const {board, turn, winner, resetGame, updateBoard} = useGameState();
 
     return (
         <main className='board'>
--- src/logic/board.js
+++ src/logic/board.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { WINNER_COMBOS, TURNS} from '../constants.js'
+import confetti from "canvas-confetti"
 
 export function useGameState() {
   const [board, setBoard] = useState(Array(9).fill(null))
@@ -8,24 +9,36 @@ export function useGameState() {
 
   const [winner, setWinner] = useState(null) //NULL: NO GANADOR, TRUE: GANADOR, FALSE: EMPATE
 
+  const updateBoard = (index) => {
+    //Si en la posición del tablero hay algo que no sea nulo o 
+    //cuando se termina la partida 
+    //cierra la ejecución.
+
+    if (board[index] || winner) return
+
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurns(newTurn);
+
+    const newWinner = checkWinnerFrom(newBoard)
+    if (newWinner) {
+        confetti()
+        setWinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+        setWinner(false)
+    }
+}
+
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurns(TURNS.X)
     setWinner(null)
   }
 
-  return {
-    board,
-    setBoard,
-    turn,
-    setTurns,
-    winner,
-    setWinner,
-    resetGame
-  }
-}
-
-export const checkWinnerFrom = (boardToCheck) => {
+  const checkWinnerFrom = (boardToCheck) => {
     //Revisamos todas las combinaciones ganadoras
     //para ver si X u O ganó
     for (const combo of WINNER_COMBOS) {
@@ -43,8 +56,17 @@ export const checkWinnerFrom = (boardToCheck) => {
     return null
   }
 
-export const checkEndGame = (newBoard) => {
+  const checkEndGame = (newBoard) => {
     //Revisamos si hay un empate
     //Si no hay más espacios vacíos
     return newBoard.every((square) => square !== null)
   }
+
+  return {
+    board,
+    turn,
+    winner,
+    resetGame,
+    updateBoard
+  }
+}
